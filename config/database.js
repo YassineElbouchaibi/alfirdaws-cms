b151c2105ca542d020f827f101c9c6c6aacab7e2
Yassine El Bouchaibi <yassine.el-bouchaibi@polymtl.ca>
2022-02-19 15:36:55 -0500

    Configured production/database.json

--- config/database.js
+++ config/database.js
@@ -1,11 +1,51 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  if (env.bool("USE_PG", false) || env("NODE_ENV") === "production") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "strapi"),
+            username: env("DATABASE_USERNAME", ""),
+            password: env("DATABASE_PASSWORD", ""),
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
