a96cf3fa73fdca00febe0b5ce041bc001151ab52
Yassine El Bouchaibi <yassine.el-bouchaibi@outlook.com>
2022-02-20 13:09:29 -0500

    Configured production/database.json

--- config/database.js
+++ config/database.js
@@ -3,22 +3,19 @@ const path = require("path");
 module.exports = ({ env }) => {
   if (env.bool("USE_PG", false) || env("NODE_ENV") === "production") {
     return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: env("DATABASE_HOST", "127.0.0.1"),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "strapi"),
-            username: env("DATABASE_USERNAME", ""),
-            password: env("DATABASE_PASSWORD", ""),
-          },
-          options: {
-            ssl: false,
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", ""),
+          password: env("DATABASE_PASSWORD", ""),
+          ssl: {
+            rejectUnauthorized: false,
           },
         },
+        debug: false,
       },
     };
   }
