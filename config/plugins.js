ef895145604e25cae1281d5a58974f520af52e68
Yassine El Bouchaibi <yassine.el-bouchaibi@outlook.com>
2022-02-27 14:03:27 -0500

    Added website builder

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,22 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  "website-builder": {
+    enabled: true,
+    config: {
+      url: env("WEBSITE_BUILDER_URL", ""),
+      headers: {
+        "Content-Type": "application/json",
+        "X-GitHub-Event": "push",
+        "X-Hub-Signature": env("WEBSITE_BUILDER_SIGNATURE", ""),
+        "X-Hub-Signature-256": env("WEBSITE_BUILDER_SIGNATURE_256", ""),
+      },
+      body: {
+        ref: "refs/heads/main",
+      },
+      trigger: {
+        type: "manual",
+      },
+    },
+  },
   graphql: {
     config: {
       endpoint: "/graphql",
@@ -11,4 +29,4 @@ module.exports = {
       },
     },
   },
-};
+});
